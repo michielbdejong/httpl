01399aebdcb7417ea62559f805c4994a13a840f3
Michiel de Jong <mbdejong@mozilla.com>
2016-02-22 10:50:30 +0100

    Cordova app

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # httpl
 Idea for including the fingerprint of a self-signed TLS certificate in a URL
+
+````bash
+npm install -g cordova
+cd cordova
+cordova add platform android
+cordova plugin add cordova-plugin-sslcertificatechecker
+cordova run android
+# in another window:
+cordova emulate android
+````
---
+++ cordova/www/js/index.js
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        runCode(function(result) {
+          document.getElementById("1").innerHTML = result;
+        });
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+function runCode(callback) {
+  var server = "https://build.phonegap.com";
+  var fingerprint = "2B 24 1B E0 D0 8C A6 41 68 C2 BB E3 60 0A DF 55 1A FC A8 45";
+
+  window.plugins.sslCertificateChecker.check(
+      successCallback,
+      errorCallback,
+      server,
+      fingerprint);
+
+  function successCallback(message) {
+    callback('success ' + message);
+    // Message is always: CONNECTION_SECURE.
+    // Now do something with the trusted server.
+  }
+
+  function errorCallback(message) {
+    callback('failure ' + message);
+    if (message == "CONNECTION_NOT_SECURE") {
+      // There is likely a man in the middle attack going on, be careful!
+    } else if (message.indexOf("CONNECTION_FAILED") >- 1) {
+      // There was no connection (yet). Internet may be down. Try again (a few times) after a little timeout.
+    }
+  }
+}
